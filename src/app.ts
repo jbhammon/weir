312d8bf172053aa4fc4de7f94729d2c5cbda1f5c
Jackson Hammond <malts18@gmail.com>
2022-03-13 16:49:01 -0400

    get prettier and eslint setup well

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,20 @@
 import express from "express";
-import { XMLParser } from 'fast-xml-parser';
-import axios from 'axios';
+import { XMLParser } from "fast-xml-parser";
+import axios from "axios";
 
 const app: express.Application = express();
 
 const parser = new XMLParser();
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello!');
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello!");
 });
 
-  
-app.get('/feed', async (req: Express.Request, res: express.Response) => {
-  const rawFeed = await axios('http://www.reddit.com/.rss');
+app.get("/feed", async (req: Express.Request, res: express.Response) => {
+  const rawFeed = await axios("http://www.reddit.com/.rss");
   const feed = parser.parse(rawFeed.data);
   console.log(feed.feed.entry[0]);
   res.send(JSON.stringify(feed));
-})
+});
 
 app.listen(5001, () => console.log("Server running"));
